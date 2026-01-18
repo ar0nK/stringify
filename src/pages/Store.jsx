7cde1e18310cd90871b9bb4ed35caa8b2bcc0d58
Ricz Attila <ricza@kkszki.hu>
2026-01-18 15:59:37 +0100

    Webshop struktúrának kialakítása

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,11 +1,38 @@
 import Card from '../components/Card.jsx'
 import NavBar from '../components/NavBar.jsx'
+import guitars from '../temp/guitars.json'
 
 export default function store() {
   return (
     <div>
     <NavBar/>
-    <Card />
+      <div className="container mt-4">
+        <div className="row">
+          <aside className="col-12 col-lg-3 mb-4">
+            <h5>Szűrők</h5>
+          </aside>
+          <section className="col-12 col-lg-9">
+            <div className="row g-5">
+              {guitars.map(guitar => (
+                <div key={guitar.id} className="col-12 col-md-4 d-flex">
+                  <Card
+                    image={guitar.image}
+                    title={guitar.title}
+                    rating={guitar.rating}
+                    maxRating={guitar.maxRating}
+                    description={guitar.description}
+                    price={guitar.price}
+                    currency={guitar.currency}
+                    onAddToCart={() =>
+                      console.log(`${guitar.title} added to cart`)
+                    }
+                  />
+                </div>
+              ))}
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   )
 }
