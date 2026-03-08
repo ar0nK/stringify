f31c7564490d7ddc08275998f62a1a0694d0c6dd
szjzsf <90899903+szjzsf@users.noreply.github.com>
2026-03-08 13:23:07 +0100

    NavBar kiegeszites

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,6 +34,14 @@ export default function NavBar() {
                 <img src={logotext} alt="Stringify" style={{ height: '2.8rem'}} />
               </a>
             </div>
+            <Link className="logo-nav-item" to="/store">
+              <img src={logoimage} alt="" />
+              Webshop
+            </Link>
+            <Link className="logo-nav-item" to="/guitar-builder">
+              <img src={logoimage} alt="" />
+              Guitár építő
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
